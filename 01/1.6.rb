c0c3e31c31bf19d047ee44c120a8917904927722
taemath <hirotaka081630@gmail.com>
2022-08-21 05:43:28 +0900

    refactor(p.51): frequent_reter_pointsを問い合わせメソッドに変更

--- 01/1.6.rb
+++ 01/1.6.rb
@@ -54,19 +54,16 @@ class Customer
 
   # レシートを作成する
   def statement
-    frequent_renter_points = 0
-
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_render_points
-
       # このレンタルの料金を表示
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
     # フッター行を追加
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
@@ -74,5 +71,9 @@ class Customer
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
