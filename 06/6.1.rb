952e69ae8e5d537f8ea49ffed2d860d6cec48f33
taemath <hirotaka081630@gmail.com>
2022-09-02 22:12:34 +0900

    feat(p.130): サンプル：ローカル変数なし
    コメントは、抽出できるメソッドを見分けるのに使えることが多い。コメント自体が抽出されたメソッドの名前の候補になる。

--- 06/6.1.rb
+++ 06/6.1.rb
@@ -19,3 +19,47 @@ class After
     puts "amount: #{amount}"
   end
 end
+
+class SampleNoLocalVariableBefore
+  def print_owing
+    outstanding = 0.0
+
+    # バナーを出力（print banner）
+    puts "**************************"
+    puts "******** Customer Owes *******"
+    puts "**************************"
+
+    # 勘定計算(caluculate outstainding)
+    @orders.each do |order|
+      outstanding += order.amount
+    end
+
+    # 詳細を表示 (print details)
+    puts "name: #{@name}"
+    puts "amount: #{outstanding}"
+  end
+end
+
+class SampleNoLocalVariableAfter
+  def print_owing
+    outstanding = 0.0
+
+    print_banner
+
+    # 勘定計算(caluculate outstainding)
+    @orders.each do |order|
+      outstanding += order.amount
+    end
+
+    # 詳細を表示 (print details)
+    puts "name: #{@name}"
+    puts "amount: #{outstanding}"
+  end
+
+  def print_banner
+    # バナーを出力
+    puts "**************************"
+    puts "******** Customer Owes *******"
+    puts "**************************"
+  end
+end
