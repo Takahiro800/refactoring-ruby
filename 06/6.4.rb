76a030d96901997597caa35c126b9349626fd18d
taemath <hirotaka081630@gmail.com>
2022-09-09 22:10:01 +0900

    feat(p.137 ~ 138): 一時変数から問い合わせメソッドへ

--- 06/6.4.rb
+++ 06/6.4.rb
@@ -1,7 +1,7 @@
 class ReplaceTempWithQuery
 end
 
-class Sample::Before
+class Before
   attr_reader :quantity, :item_price
 
   def before
@@ -15,7 +15,7 @@ class Sample::Before
   end
 end
 
-class Sample::After
+class After
   attr_reader :quantity, :item_price
 
   def after
@@ -25,6 +25,45 @@ class Sample::After
       base_price * 0.98
     end
   end
+
+  def base_price
+    @quantity * @item_price
+  end
+end
+
+# 簡単なメソッドから始める
+class SampleBefore < ReplaceTempWithQuery
+  attr_reader :quantity, :item_price
+
+  def price
+    base_price = @quantity * @item_price
+
+    if base_price < 1000
+      discount_factor = 0.95
+    else
+      discount_factor = 0.98
+    end
+
+    base_price * discount_factor
+  end
+end
+
+# まず、代入の右辺を抜き出してメソッドにまとめる
+class SampleStep1 < ReplaceTempWithQuery
+  attr_reader :quantity, :item_price
+
+  def price
+    a_base_price = base_price
+
+    if a_base_price > 1000
+      discount_factor = 0.95
+    else
+      discount_factor = 0.98
+    end
+    a_base_price * discount_factor
+  end
+
+  private
 
   def base_price
     @quantity * @item_price
