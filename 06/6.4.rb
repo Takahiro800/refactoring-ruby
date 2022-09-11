d3f11707af6ee0a984624c3ebb55446c72a80018
taemath <hirotaka081630@gmail.com>
2022-09-11 21:50:31 +0900

    refactor(p.139): 一時変数から問い合わせメソッドへ（一時変数の削除）

--- 06/6.4.rb
+++ 06/6.4.rb
@@ -93,3 +93,22 @@ class SampleStep2 < ReplaceTempWithQuery
     @quantity * @item_price
   end
 end
+
+# 一時変数の削除
+class SmapleStep3 < ReplaceTempWithQuery
+  attr_reader :quantity, :item_price
+
+  def price
+    if base_price > 1000
+      discount_factor = 0.95
+    else
+      discount_factor = 0.98
+    end
+
+    base_price * discount_factor
+  end
+
+  def base_price
+    @quantity * @item_price
+  end
+end
