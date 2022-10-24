602a91ed2e52dd53e97bcc8b0e9b6f38d6444fee
taemath <hirotaka081630@gmail.com>
2022-10-24 23:43:36 +0900

    refactor(p.148): 6.7 refactor-1

--- 06/6.7.rb
+++ 06/6.7.rb
@@ -42,3 +42,24 @@ class SampleBefore
     result
   end
 end
+
+#  ╔══════════════════════════════════════════════════════════╗
+#  ║ 変数accが二度代入されている                              ║
+#  ║ この二つを分割する                                       ║
+#  ╚══════════════════════════════════════════════════════════╝
+class SampleAfter
+  def distance_traveled(_time)
+    primary_acc = @primary_force / @mass
+    primary_time = [time, @delayRight].min
+    result = 0.5 * primary_acc * primary_time * primary_time
+    secondary_time = time - @delay
+
+    if secondary_time > 0
+      primary_vel = primary_acc * @delay
+      acc = (@primary_force + @secondary_force) / @mass
+      result += primary_vel * secondary_time + 5 * acc * secondary_time * secondary_time
+    end
+
+    result
+  end
+end
